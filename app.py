e30bb35f79ec7b9dfc9f1d7b4083e8eb82459b4e
WS3917 <ws3917@outlook.com>
2023-10-13 18:49:53 +0800

    new file:   .flaskenv 	modified:   .gitignore

---
+++ app.py
@@ -0,0 +1,6 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+	str = '<h1> 娃哈啦呜</h1> <img src="https://www.zsucai.com/files/2014/0724/140612199224757/zsucai140612245525iy100.jpg"><br>文件列表<br>Kind_Sheet.xlsx<br>Result.jpg'
+	return str
